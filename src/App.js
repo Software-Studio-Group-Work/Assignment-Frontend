14673058692f3ee92ef9e1874428001314bde70a
dinkxz <dink.4567@gmail.com>
2022-04-20 22:37:02 +0700

    user

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./layouts/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import User from "./pages/User/User";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} /> 
         <Route path="/register" element={<Register />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   return (
     <div id="login">
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3">
         <Form.Label id='title-login'>ลงชื่อเข้าใช้งาน</Form.Label><br/>
         <Form.Control  type="email-user-id" placeholder="อีเมล/ชื่อผู้ใช้" id="input-login"  /><br/>
         <Form.Control  type="password" placeholder="รหัสผ่าน" id="input-login" />
