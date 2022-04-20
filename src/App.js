d5cc7dcfe2b0f3659c02079a3dcc2cea7acf8f76
dinkxz <dink.4567@gmail.com>
2022-04-20 20:53:46 +0700

    login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./layouts/NavBar/NavBar";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
