2be63c04067b1f525ad4a4005336aa9f7ee1794e
dinkxz <dink.4567@gmail.com>
2022-04-20 21:47:43 +0700

    login & register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Form,Button } from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
+import './Login.css';
 function Login() {
 
    const navigate = useNavigate();
 
   return (
+    <div id="login">
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>ลงชื่อเข้าใช้งาน</Form.Label><br/>
-        <Form.Control  type="email-user-id" placeholder="อีเมล/ชื่อผู้ใช้"  /><br/>
-        <Form.Control  type="password" placeholder="รหัสผ่าน" />
+        <Form.Label id='title-login'>ลงชื่อเข้าใช้งาน</Form.Label><br/>
+        <Form.Control  type="email-user-id" placeholder="อีเมล/ชื่อผู้ใช้" id="input-login"  /><br/>
+        <Form.Control  type="password" placeholder="รหัสผ่าน" id="input-login" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" id="button-login">
     Login
   </Button><br/>
-  <Form.Label>ยังไม่ได้เป็นสมาชิก</Form.Label>
-  <span onClick={()=>navigate("/register")} >SignUpก</span>
+  <Form.Label id='text-login'>ยังไม่ได้เป็นสมาชิก</Form.Label>
+  <span id='text-signup' onClick={()=>navigate("/register")} >Sign Up</span>
     </Form>
+    </div>
   );
 }
 
