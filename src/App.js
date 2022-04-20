c219343226f7ede7e80b846f2662be0cd54bab3c
phlpat <patphonlapat@gmail.com>
2022-04-20 22:18:04 +0700

    add layout page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./layouts/NavBar/NavBar";
 import Home from "./pages/Home/Home";
+import Post from "./pages/Home/Post";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} /> 
         <Route path="/register" element={<Register />} />
+        <Route path="/post" element={<Post/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Home/Post.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+function Post() {
+  return (
+    <div>
+      <div
+        class="container card"
+        style={{
+          marginTop: "20px",
+          borderRadius: "10px",
+          backgroundColor: "#c9bc8e",
+        }}
+      >
+        <div class="card-body">
+          <div class="container card" style={{ borderRadius: "10px" }}>
+            <div class="container form-group">
+              <label for="InputQuestion" style={{ marginTop: "10px" }}>
+                <strong>1.ระบุคำถามของคุณ</strong>
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="question"
+                aria-describedby=""
+                placeholder=""
+                style={{ backgroundColor: "#c4c4c4" }}
+              ></input>
+            </div>
+          </div>
+
+          <div
+            class="container card"
+            style={{ borderRadius: "10px", marginTop: "10px" }}
+          >
+            <div class="container form-group">
+              <label for="InputQuestionDetail" style={{ marginTop: "10px" }}>
+                <strong>2.เขียนรายละเอียดของคำถาม</strong>
+              </label>
+              <textarea
+                type="text"
+                class="form-control"
+                id="questionDetail"
+                aria-describedby=""
+                placeholder=""
+                rows="10"
+                style={{ backgroundColor: "#c4c4c4" }}
+              ></textarea>
+            </div>  
+          </div>
+          <div
+            class="container card"
+            style={{ borderRadius: "10px", marginTop: "10px" }}
+          >
+            <div class="container form-group"  style={{ marginTop: "10px" }}>
+                <label><strong>เพิ่มรูปภาพ</strong></label>
+              <input type="file" class="form-control" style={{border:"none"}} ></input>
+            </div>  
+          </div>
+        </div>
+      </div>
+      <div
+        class="container"
+        style={{ borderRadius: "10px", marginTop: "10px" }}
+      >
+        <button type="submit" class="btn btn-success mb-2">
+          ส่งกระทู้
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
