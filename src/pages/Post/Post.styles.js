7f29a90741d1eaa7fa2f3fcf2523fa083f47c9c1
pjpure <pure254326@gmail.com>
2022-04-22 22:10:40 +0700

    update post page add post item and comment item

---
+++ src/pages/Post/Post.styles.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 10px;
+  margin-top: 20px;
+`;
+
+export const PostItem = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  background-color: #bca65e;
+  border-radius: 10px;
+  padding: 15px;
+  margin-bottom: 20px;
+  position: relative;
+
+  h4 {
+    color: #fff;
+  }
+  img {
+    width: 50%;
+    max-height: 300px;
+    margin: 20px auto;
+    object-fit: contain;
+  }
+  p {
+    color: #fff;
+  }
+`;
+
+export const CommentItem = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  background-color: #c9bc8e;
+  border-radius: 10px;
+  padding: 15px;
+  p {
+    color: #fff;
+  }
+`;
+
+export const FooterItem = styled.div`
+  color: #fff;
+  position: relative;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  column-gap: 20px;
+  align-items: center;
+
+  svg {
+    cursor: pointer;
+  }
+`;
