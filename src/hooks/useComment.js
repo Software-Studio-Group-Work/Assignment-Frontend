69cf62a68ad1a065058c6aaccc1c5ed61c509339
pjpure <pure254326@gmail.com>
2022-04-25 16:33:50 +0700

    update comment api

--- src/hooks/useComment.js
+++ src/hooks/useComment.js
@@ -1,54 +1,64 @@
 import httpClient from "../utils/httpClient";
 import { useQuery, useMutation, useQueryClient } from "react-query";
 
-export const useGetComments = (postId) => {
+export const useGetCommentsByPostId = (postId) => {
   return useQuery(["comments", postId], async () => {
-    const { data } = await httpClient.get(`comments?postId=${postId}`);
+    const { data } = await httpClient.get(
+      `Comment/GetCommentsByPost/${postId}`
+    );
     return data;
   });
 };
 
-export const useAddComment = (postId) => {
-  const queryClient = useQueryClient();
-  return useMutation(
-    async (comment) => {
-      const { data } = await httpClient.post("comments", comment);
-      return data;
-    },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(["comments", postId]);
-      },
-    }
-  );
+export const useGetComment = (commentId) => {
+  return useQuery(["comment", commentId], async () => {
+    const { data } = await httpClient.get(`Comment/GetOneComment/${commentId}`);
+    return data;
+  });
 };
 
-export const useUpdateComment = (id) => {
+export const useAddComment = () => {
   const queryClient = useQueryClient();
   return useMutation(
     async (comment) => {
-      const { data } = await httpClient.put(`comments/${id}`, comment);
+      const { data } = await httpClient.post(
+        "Comment/CreateOneComment",
+        comment
+      );
       return data;
     },
     {
-      onSuccess: () => {
+      onSuccess: (comment) => {
         queryClient.invalidateQueries(["comments", comment.postId]);
       },
     }
   );
 };
 
-export const useDeleteComment = (id) => {
+export const useUpdateComment = () => {
   const queryClient = useQueryClient();
-  return useMutation(
-    async () => {
-      const { data } = await httpClient.delete(`comments/${id}`);
-      return data;
-    },
-    {
-      onSuccess: (data) => {
-        queryClient.invalidateQueries(["comments", data.postId]);
-      },
+  return useMutation(async (comment) => {
+    const { data } = await httpClient.put(
+      `Comment/UpdateOneComment/${comment.id}`,
+      comment
+    );
+    if (data) {
+      queryClient.invalidateQueries(["comments", comment.postId]);
     }
-  );
+
+    return data;
+  });
+};
+
+export const useDeleteComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation(async (comment) => {
+    const { data } = await httpClient.delete(
+      `Comment/DeleteOneComment/${comment.id}`
+    );
+    if (data) {
+      queryClient.invalidateQueries(["comments", comment.postId]);
+    }
+    return data;
+  });
 };
--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import {
   PostContainer,
@@ -6,6 +6,7 @@ import {
   CommentItem,
   FooterItem,
   Header,
+  CommentForm,
 } from "./Post.styles";
 import { AiFillDelete } from "react-icons/ai";
 import { BiHide } from "react-icons/bi";
@@ -14,16 +15,27 @@ import { AiFillEdit } from "react-icons/ai";
 import { useGetPost } from "../../hooks/usePost";
 import { UserContext } from "../../contexts/UserContext";
 import { useDeletePost, useUpdatePost } from "../../hooks/usePost";
+import { Button } from "react-bootstrap";
+import {
+  useGetCommentsByPostId,
+  useAddComment,
+  useUpdateComment,
+  useDeleteComment,
+} from "../../hooks/useComment";
 function Post() {
   const navigate = useNavigate();
+  const [comment, setComment] = useState("");
+  const [commentEdit, setCommentEdit] = useState(null);
   const { user } = useContext(UserContext);
   const { id } = useParams();
   const { data, isLoading, isError } = useGetPost(id);
+  const { data: comments } = useGetCommentsByPostId(id);
   const { mutate: deletePost } = useDeletePost();
   const { mutate: updatePost } = useUpdatePost();
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error...</div>;
-  if (data?.isHide) return <h2>กระทู้นี้ถูกซ่อนแล้ว</h2>;
+  const { mutate: addComment } = useAddComment();
+  const { mutate: updateComment } = useUpdateComment();
+  const { mutate: deleteComment } = useDeleteComment();
+  const [isEditComment, setIsEditComment] = useState(false);
 
   const onDelete = () => {
     if (window.confirm("คุณต้องการลบกระทู้นี้หรือไม่?")) {
@@ -40,6 +52,49 @@ function Post() {
       navigate("/");
     }
   };
+
+  const onSubmitComment = (e) => {
+    e.preventDefault();
+    if (isEditComment) {
+      if (comment.trim() === "") return;
+      let newComment = {
+        ...commentEdit,
+        comment: comment,
+      };
+      updateComment(newComment);
+    } else {
+      if (comment.trim() === "") return;
+      let newComment = {
+        postId: id,
+        userId: user.id,
+        comment,
+      };
+      addComment(newComment);
+    }
+    setComment("");
+    setIsEditComment(false);
+  };
+
+  const onEditClick = (comment) => {
+    setIsEditComment(true);
+    setCommentEdit(comment);
+    setComment(comment.comment);
+  };
+
+  const onDeleteComment = (comment) => {
+    if (window.confirm("คุณต้องการลบความคิดเห็นนี้หรือไม่?")) {
+      deleteComment(comment);
+    }
+  };
+
+  const onCancelCommentClick = () => {
+    setIsEditComment(false);
+    setComment("");
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+  if (data?.isHide) return <h2>กระทู้นี้ถูกซ่อนแล้ว</h2>;
 
   return (
     <PostContainer>
@@ -69,21 +124,57 @@ function Post() {
           <span>สมาชิกหมายเลข: {data?.userId}</span>
         </FooterItem>
       </PostItem>
-      <CommentItem>
-        <Header>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <div className="icons">
-            <BiHide />
-            <AiFillEdit />
-            <AiFillDelete />
-          </div>
-        </Header>
-        <FooterItem>
-          <AiFillLike />
-          <span>|</span>
-          <span>สมาชิกหมายเลข: User</span>
-        </FooterItem>
-      </CommentItem>
+      {comments?.map((comment) => (
+        <CommentItem key={comment.id}>
+          <Header>
+            <p>{comment.comment}</p>
+            {user && comment.userId !== user?.id && user?.role === "admin" && (
+              <div className="icons">
+                <AiFillDelete onClick={() => onDeleteComment(comment)} />
+              </div>
+            )}
+
+            {user && comment.userId === user?.id && (
+              <div className="icons">
+                <AiFillEdit onClick={() => onEditClick(comment)} />
+                <AiFillDelete onClick={() => onDeleteComment(comment)} />
+              </div>
+            )}
+          </Header>
+          <FooterItem>
+            <AiFillLike />
+            <span>|</span>
+            <span>สมาชิกหมายเลข: {comment.userId}</span>
+          </FooterItem>
+        </CommentItem>
+      ))}
+
+      <CommentForm onSubmit={onSubmitComment}>
+        <label style={{ marginTop: "10px" }}>
+          <strong>แสดงความคิดเห็น</strong>
+        </label>
+        <textarea
+          type="text"
+          className="form-control"
+          id="questionDetail"
+          aria-describedby=""
+          placeholder=""
+          rows="2"
+          style={{ backgroundColor: "#c4c4c4" }}
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <div>
+          <Button variant="warning" type="submit">
+            {isEditComment ? "บันทึก" : "แสดงความคิดเห็น"}
+          </Button>
+          {isEditComment && (
+            <Button variant="danger" onClick={onCancelCommentClick}>
+              ยกเลิก
+            </Button>
+          )}
+        </div>
+      </CommentForm>
     </PostContainer>
   );
 }
